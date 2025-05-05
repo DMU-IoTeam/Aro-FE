11de0d56d84badd6fea2a50ccee338ed5beeb88c
PC <pc@PCui-MacBookAir.local>
2025-05-06 06:20:07 +0900

    init: project init

---
+++ screen/FallDetectionScreen.tsx
@@ -0,0 +1,12 @@
+import {Text} from '@react-navigation/elements';
+import {View} from 'react-native';
+
+const FallDetectionScreen = () => {
+  return (
+    <View>
+      <Text>메인~</Text>
+    </View>
+  );
+};
+
+export default FallDetectionScreen;
---
+++ screen/MainScreen.tsx
@@ -0,0 +1,12 @@
+import {Text} from '@react-navigation/elements';
+import {View} from 'react-native';
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>메인~</Text>
+    </View>
+  );
+};
+
+export default MainScreen;
