596e900bb6d09def398ae35119a19ea0bae7cd72
doyeon <duseh729@naver.com>
2025-05-22 04:04:13 +0900

    feat: 회원가입 스크린 구현

--- screen/SignupScreen.tsx
+++ screen/SignupScreen.tsx
@@ -1,7 +1,34 @@
+import {View} from 'react-native';
 import Container from '../layouts/Container';
+import Input from '../components/common/Input';
+import CommonButton from '../components/common/CommonButton';
+import {useState} from 'react';
 
 const SignupScreen = () => {
-  return <Container>회원가입</Container>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  return (
+    <Container style={{gap: 16}}>
+      <View style={{flexDirection: 'row', gap: 4}}>
+        <Input placeholder={'이메일'} style={{flex: 1}} />
+        <CommonButton>중복 확인</CommonButton>
+      </View>
+
+      <Input placeholder="비밀번호"></Input>
+
+      <Input placeholder="비밀번호 확인"></Input>
+
+      <View style={{flexDirection: 'row', gap: 4}}>
+        <Input placeholder={'핸드폰 번호'} style={{flex: 1}} />
+        <CommonButton>인증</CommonButton>
+      </View>
+
+      <CommonButton>회원가입</CommonButton>
+    </Container>
+  );
 };
 
 export default SignupScreen;
