f4461fcc8cefeab23c1f7518fc357fec01fe9914
doyeon <duseh729@naver.com>
2025-05-21 17:08:44 +0900

    feat: 로봇 상태 스크린

--- constants/color.js
+++ constants/color.js
@@ -1,6 +1,7 @@
 const COLOR = {
     DEFAULT_COLOR : "#FF7E1B",
-    GRAY900: "gray"
+    GRAY900: "gray",
+    GRAY700: "#a3a3a3"
 }
 
 export default COLOR;
--- screen/RobotConditionScreen.tsx
+++ screen/RobotConditionScreen.tsx
@@ -1,7 +1,47 @@
-import { Text, View } from "react-native";
+import {Text, View} from 'react-native';
+import Container from '../layouts/Container';
+import layout from '../constants/layout';
+import COLOR from '../constants/color';
 
-const RobotConditionScreen = ()=>{
-    return <View><Text>로봇 상태</Text></View>
-}
+const RobotConditionScreen = () => {
+  const robotConditionData = [
+    {
+      key: '배터리',
+      value: `${88}%`,
+    },
+    {
+      key: '시리얼넘버',
+      value: 'q1w2e3r4t5',
+    },
+  ];
+
+  return (
+    <Container>
+      <View
+        style={{
+          borderRadius: layout.BORDER_RADIUS,
+          borderWidth: 1,
+          borderColor: COLOR.GRAY900,
+          padding: layout.PADDING_VALUE,
+        }}>
+        {robotConditionData.map((item, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                paddingBottom: index !== robotConditionData.length - 1 && 6,
+                paddingTop: index === robotConditionData.length - 1 && 6,
+                borderBottomWidth: index !== robotConditionData.length - 1 && 1,
+                borderColor: COLOR.GRAY700,
+              }}>
+              <Text style={{fontWeight: 700}}>{item.key}</Text>
+              <Text style={{fontWeight: 700}}>{item.value}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </Container>
+  );
+};
 
 export default RobotConditionScreen;
